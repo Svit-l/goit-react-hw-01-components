38cdd135739dd2a15d30d89a7fe3f3308ce7679f
Svit-l <lov.svit@gmail.com>
2022-01-09 17:15:57 +0200

    three tasks styles

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -2,9 +2,11 @@ import PropTypes from 'prop-types';
 import s from './Friends.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
+  const isOnlineColor = isOnline ? s.status : s.status__false;
+
   return (
     <li className={s.item}>
-      <span className={s.status}>{isOnline}</span>
+      <span className={isOnlineColor}>{isOnline}</span>
       <img
         className={s.avatar}
         src={avatar}
